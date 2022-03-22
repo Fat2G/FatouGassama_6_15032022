80c5e2ec2380b89b16025dbd50a365febc519976
Fat2G <fatougassa@gmail.com>
2022-03-22 19:49:12 +0100

    implementation fonction login

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -17,6 +17,28 @@ exports.signup = (req, res, next) => {
         .then(() => res.status(201).json({ message: 'Utilisateur créé !'}))
         .catch(err => res.status(400).json({ error }))
     })
-    .catch(err => res.status(500).json({ error }))
+    .catch(err => res.status(500).json({ error }));
 };
 
+//fonction pour la connection des utilisateurs existants
+exports.login = (req, res, next) => {
+  //explication 3.4
+  User.findOne({ email: req.body.email})
+    .then(user => {
+      if (!user){
+        return res.status(401).json({ message: 'Utilisateur non trouvé !'});
+      }
+      bcrypt.compare(req.body.password, user.password)
+        .then(valid => {
+          if (!valid) {
+            return res.status(401).json({ message: 'Mot de passe incorrect !'});
+          }
+          res.status(200).json({
+            userId: user._id,
+            token: 'TOKEN'
+          })
+        })
+        .catch(err => res.status(500).json({ error}))
+    })
+    .catch(err => res.status(500).json({ error}));
+};
