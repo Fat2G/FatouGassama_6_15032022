8e23094214cef9301fff0e4ad2d67eb38876294a
Fat2G <fatougassa@gmail.com>
2022-03-22 20:03:45 +0100

    encodage token

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,7 @@
 //import du package bcrypt
 const bcrypt = require('bcrypt');
+//import du package jsonwebtoken
+const jwt = require('jsonwebtoken');
 
 //import du modèle users
 const User= require('../models/user');
@@ -35,10 +37,14 @@ exports.login = (req, res, next) => {
           }
           res.status(200).json({
             userId: user._id,
-            token: 'TOKEN'
+            token: jwt.sign(
+              { userId: user._id},
+              'RANDOM_TOKEN_SECRET',
+              {expiresIn: '24h'}
+            )
           })
         })
-        .catch(err => res.status(500).json({ error}))
+        .catch(err => res.status(500).json({ error}));
     })
     .catch(err => res.status(500).json({ error}));
 };
