ddb850cf10f523bc44259f83bb08b154644d76a3
Fat2G <fatougassa@gmail.com>
2022-03-31 01:38:35 +0200

    modif modèle de données

--- backend/models/thing.js
+++ backend/models/thing.js
@@ -7,9 +7,7 @@ const thingSchema = mongoose.Schema({
   description: { type: String, required: true},
   mainPepper: { type: String, required: true},
   imageUrl: { type: String, required: true},
-  heat: { type: number, required: true},
-  usersLiked: [{ type: Schema.Types.ObjectId, ref: 'User'}], 
-  usersDisliked: [{ type: Schema.Types.ObjectId, ref: 'User'}]
+  heat: { type: number, required: true}
 });
 
 module.exports = mongoose.model('Thing', thingSchema);
