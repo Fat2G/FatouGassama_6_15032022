1d17e21e2415db35043bdbed3d2ebd86849fe9f5
Fat2G <fatougassa@gmail.com>
2022-03-31 01:37:43 +0200

    création fichiers sauces controllers

---
+++ backend/controllers/sauces.js
@@ -0,0 +1,25 @@
+const Thing = require('../models/thing');
+
+exports.createThing = (req, res, next) => {
+  delete req.body._id;
+  const thing = new Thing({    
+    ...req.body
+  })
+  thing.save()
+    .then(() => res.status(201).json({ message: 'Sauce enregistrée !'}))
+    .catch(error => res.status(400).json({ error }));
+  next();
+};
+
+exports.getOneThing = (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then(thing => res.status(200).json(thing))
+    .catch(error => res.status(404).json({ error}))
+};
+
+exports.getAllThings = (req, res, next) => {
+  Thing.find()
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({ error }));
+  next();
+};
