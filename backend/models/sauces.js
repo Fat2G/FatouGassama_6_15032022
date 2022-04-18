942266c1d8120d1f8c06a89d0562dba624425ca1
Fat2G <fatougassa@gmail.com>
2022-04-19 00:23:36 +0200

    ajout éléments dans schéma mongoose de la sauce

--- backend/models/sauces.js
+++ backend/models/sauces.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
 const thingSchema = mongoose.Schema({
   userId: { type: String, required: true},
@@ -7,7 +8,11 @@ const thingSchema = mongoose.Schema({
   description: { type: String, required: true},
   mainPepper: { type: String, required: true},
   imageUrl: { type: String, required: true},
-  heat: { type: Number, required: true}
+  heat: { type: Number, required: true},
+  likes : { type: Number, default: 0, required: false},
+  dislikes : { type: Number, default: 0, required: false},
+  usersLiked: [{ type: Schema.Types.ObjectId, ref: 'User'}], 
+  usersDisliked: [{ type: Schema.Types.ObjectId, ref: 'User'}]
 });
 
 module.exports = mongoose.model('Thing', thingSchema);
