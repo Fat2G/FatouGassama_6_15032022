5e56afd3e8085a29a96a098e0b61260868d8888f
Fat2G <fatougassa@gmail.com>
2022-04-25 22:31:26 +0200

    import des middleware de controles

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,13 +1,20 @@
-//import d'express pour créer un routeur
+// import d'express pour créer un routeur
 const express = require('express');
+// création du routeur
 const router = express.Router();
 
-//association des fonctions aux différentes routes
+// import du controller 
 const userCtrl = require('../controllers/user');
+// import du middleware de contrôle du mot de passe
+const password = require('../middleware/password');
+// import du middleware de contrôle de l'adresse mail
+const email = require('../middleware/email');
+// import du middleware de tentatives de Connexion
+const connection = require('../middleware/connection');
 
 //création de routes POST
-router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/signup', email, password, userCtrl.signup);
+router.post('/login', connection, userCtrl.login);
 
-//export du routeur
+// export du routeur
 module.exports = router;
