ce1c683574c1f454b4ab0322c7186c7221e4225a
Fat2G <fatougassa@gmail.com>
2022-04-26 07:00:39 +0200

    utilisation des variables d'environnement

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,12 +1,15 @@
 //import du package jsonwebtoken pour encodage des tokens
 const jwt = require('jsonwebtoken');
 
+// variables d'environnement pour les odnnées sensibles
+require('dotenv').config();
+
 //export du middleware d'authentification
 module.exports = (req, res, next) => {
   try {
     // récupération du token 
     const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+    const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
     const userId = decodedToken.userId;
     // vérification de l'ID de l'utilisateur par rapport à celui du corps de la requête
     if (req.body.userId && req.body.userId !== userId) {
