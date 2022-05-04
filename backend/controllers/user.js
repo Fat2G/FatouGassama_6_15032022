b3874756d8bd67dca8a7dc0d2b3d4cc6c621163a
Fat2G <fatougassa@gmail.com>
2022-05-04 02:14:39 +0200

    version corrigée

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,3 +1,6 @@
+/*// utilisation des variables d'environnement pour cacher les données sensibles
+require('dotenv').config();
+
 //import du package bcrypt
 const bcrypt = require('bcrypt');
 //import du package jsonwebtoken
@@ -5,55 +8,49 @@ const jwt = require('jsonwebtoken');
 //import du package crypto-js pour chiffrer le mail
 const cryptoJs = require('crypto-js');
 
-//import du modèle users
+//import du modèle utilisateur
 const User= require('../models/user');
-
-// utilisation des variables d'environnement pour cacher les données sensibles
-require('dotenv').config();
-const cryptoKey = process.env.CRYPTOJS_KEY_EMAIL;
-const jwtKey = process.env.JWT_KEY;
 
 //fonction pour l'enregistrement des utilisateurs
 exports.signup = (req, res, next) => {
   // chiffrage de l'email 
-  const emailCryptoJs = cryptoJs.SHA256(req.body.email, `${cryptoKey}` ).toString();
-  console.log("emailCryptoJs");
-  console.log(emailCryptoJs);
-
-  // hash du mot de passe
+  //const emailCryptoJs = cryptoJs.SHA256(req.body.email, process.env.CRYPTOJS_KEY_EMAIL).toString();
+  // hashage du mot de passe
   bcrypt.hash(req.body.password, 10)
     .then(hash => {
       const user = new User({
-        email: emailCryptoJs,
+        email: req.body.email,
         password: hash
       });
-      // Et si le mail est déjà utilisé ?
+      // Enregistrement de l'utilisateur dans la base de données
       user.save()
         .then(() => res.status(201).json({ message: 'Utilisateur créé !'}))
-        .catch(error => res.status(400).json({ error }))
+        .catch(error => res.status(400).json({ message : 'Utilisateur déjà existant !' }))
     })
     .catch(error => res.status(500).json({ error }));
 };
 
 //fonction pour la connexion des utilisateurs existants
 exports.login = (req, res, next) => {
-  const emailCryptoJs = cryptoJs.SHA256(req.body.email, `${cryptoKey}` ).toString(); 
-  User.findOne({ email: emailCryptoJs})
+  // const emailCryptoJs = cryptoJs.SHA256(req.body.email, process.env.CRYPTOJS_KEY_EMAIL).toString(); 
+  // recherche de l'email encryptée de l'utilisateur
+  User.findOne({ email: req.body.email})
     .then(user => {
       if (!user){
         return res.status(401).json({ message: 'Utilisateur non trouvé !'});
       }
+      //comparaison du mot de passe tapé à celui de la base de données
       bcrypt.compare(req.body.password, user.password)
         .then(valid => {
           if (!valid) {
             return res.status(401).json({ message: 'Mot de passe incorrect !'});
           }
           res.status(200).json({
-            //encodage du userId pour lier les données à l'utilisateur
+            //Le mot de passe correspond et l'utilisateur reçoit un token encodé d'une durée de 12h
             userId: user._id,
             token: jwt.sign(
               { userId: user._id},
-              `${jwtKey}`,
+              process.env.JWT_KEY,
               {expiresIn: '12h'}
             )
           })
@@ -61,4 +58,68 @@ exports.login = (req, res, next) => {
         .catch(error => res.status(500).json({ error}));
     })
     .catch(error => res.status(500).json({ error}));
+};*/
+
+// IMPORTS
+// utilisation des variables d'environnement pour cacher les données sensibles
+require("dotenv").config(); // Importation du package 'dotenv'
+//import du modèle utilisateur
+const User = require("../models/User");
+//import du package bcrypt
+const bcrypt = require("bcrypt"); 
+//import du package jsonwebtoken pour l'encodage des tokens
+const jwt = require("jsonwebtoken");
+
+//fonction pour l'enregistrement des utilisateurs
+exports.signup = (req, res, next) => {
+  // hashage du mot de passe
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      // Création du nouvel utilisateur
+      const user = new User({
+        email: req.body.email,
+        password: hash,
+      });
+      // Enregistrement de l'utilisateur dans la base de données
+      user.save()
+        .then(() => {
+          res.status(201).json({ message: "Utilisateur crée !" });
+        })
+        .catch( error => res.status(400).json({ error })
+        );
+    })
+    .catch(error => res.status(500).json({ error }));
+};
+
+//fonction pour la connexion des utilisateurs existants
+exports.login = (req, res, next) => {
+  // recherche de l'utilisateur dans la base de données
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      // si utilisateur non trouvé
+      if (!user) {
+        return res.status(401).json({ message: "Utilisateur non trouvé !" });
+      }
+      // si utilisateur trouvé
+      // comparaison du mot de passe envoyé avec celui hashé dans la base de données
+      bcrypt.compare(req.body.password, user.password)
+        .then( valid => {
+          // si le mot de passe ne correspond pas
+          if (!valid) {
+            return res.status(401).json({ message: "Mot de passe incorrect !" });
+          }
+          // si le mot de passe correspond et l'utilisateur reçoit un token encodé d'une durée de 12h
+          res.status(200).json({
+            userId: user._id,
+            token: jwt.sign(
+              { userId: user._id },
+              process.env.SECRET_TOKEN,
+              { expiresIn: "12h" }
+            )
+          })
+        })
+        .catch( error => res.status(500).json({ error }));
+    })
+    .catch( error => res.status(500).json({ error }));
 };
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,23 +1,25 @@
+// variables d'environnement pour les données sensibles
+require('dotenv').config();
+
 //import du package jsonwebtoken pour encodage des tokens
 const jwt = require('jsonwebtoken');
-
-// variables d'environnement pour les odnnées sensibles
-require('dotenv').config();
 
 //export du middleware d'authentification
 module.exports = (req, res, next) => {
   try {
     // récupération du token 
     const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
+    // Décodage du token avec pour arguments le token à vérifier et la clé d'encodage
+    const decodedToken = jwt.verify(token, process.env.SECRET_TOKEN);
+    // Extraire le 'userId' qui est à l'intérieur
     const userId = decodedToken.userId;
     // vérification de l'ID de l'utilisateur par rapport à celui du corps de la requête
     if (req.body.userId && req.body.userId !== userId) {
-      throw 'User ID non valable!';
+      throw 'User ID non valable';
     } else {
       next();
     }
   } catch (error) {
-    res.status(401).json({ error: error | 'Requête non authentifiée !'})
+    res.status(401).json({ error });
   }
 };
